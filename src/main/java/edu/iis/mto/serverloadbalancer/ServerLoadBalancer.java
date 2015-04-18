1557bda1e00e78759cbc0ff59236df5cdb3625e1
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-04-18 15:20:45 +0200

    test 5 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,8 +4,13 @@ public class ServerLoadBalancer {
 
 	public void balance(final Server[] servers, final Vm[] vms) {
 		for (Vm vm : vms) {
-			servers[0].addVm(vm);
+			Server lessLoaded = null;
+			for (Server server : servers) {
+				if (lessLoaded == null || lessLoaded.getCurrentLoadPercentage() > server.getCurrentLoadPercentage()) {
+					lessLoaded = server;
+				}
+			}
+			lessLoaded.addVm(vm);
 		}
 	}
-
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/builders/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/builders/ServerBuilder.java
@@ -1,10 +1,12 @@
 package edu.iis.mto.serverloadbalancer.builders;
 
 import edu.iis.mto.serverloadbalancer.Server;
+import edu.iis.mto.serverloadbalancer.Vm;
 
 public class ServerBuilder implements Builder<Server> {
 
 	int capacity;
+	double initialLoad;
 
 	public ServerBuilder() {
 		withCapacity(1);
@@ -16,11 +18,21 @@ public class ServerBuilder implements Builder<Server> {
 	}
 
 	public Server build() {
-		return new Server(capacity);
+		Server server = new Server(capacity);
+		if (initialLoad > 0) {
+			Vm vm = new VmBuilder().ofSize((int) (initialLoad / 100.0d * (double) capacity)).build();
+			server.addVm(vm);
+		}
+		return server;
 	}
 
 	public static ServerBuilder server() {
 		return new ServerBuilder();
+	}
+
+	public ServerBuilder withCurrentLoadOf(double initialLoad) {
+		this.initialLoad = initialLoad;
+		return this;
 	}
 
 }
