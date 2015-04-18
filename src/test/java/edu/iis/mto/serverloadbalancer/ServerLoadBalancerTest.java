2d4f885b17bd4696822b704f0f5c46da3b879e9e
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-04-18 14:23:26 +0200

    test 3 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -37,6 +37,17 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasLoadPercentageOf(100.0d));
 		assertThat("the server contains vm", theServer.contains(theVm));
 	}
+	
+	@Test 
+	public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillTheServerWithTenPercent(){
+		Server theServer = a(server().withCapacity(10));
+		Vm theVm = a(vm().ofSize(1));
+		balance(anArrayOfServersWith(theServer), aArrayOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(10.0d));
+		assertThat("the server should contain vm", theServer.contains(theVm));
+		
+	}
 
 	private <T> T a(Builder<T> builder) {
 		return builder.build();
